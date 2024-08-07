af551e90a916ae454588d9fd9543cc407be14f6d
dangooddd <dangoodds@gmail.com>
2024-08-08 01:15:06 +0300

    chore: set VISUAL env variable to hx

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -2,9 +2,10 @@
 
 # to avoid some shit
 if command -vq hx
+    set -x VISUAL hx
     set -x EDITOR hx
 end
 
 set -x PAGER less
 set -x LESS "--tilde -RXS"
-set -x RIPGREP_CONFIG_PATH "$HOME/.config/ripgrep/ripgrep.conf"
+set -x RIPGREP_CONFIG_PATH "$HOME/.config/ripgrep/config"
