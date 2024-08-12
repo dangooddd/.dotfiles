d4afbdf21e24396cfaab647d5f08be7b349146a0
dangooddd <dangoodds@gmail.com>
2024-08-12 18:24:57 +0300

    fix: change some variables in fish config

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -8,14 +8,14 @@ end
 
 # rustup
 if test -z $RUSTUP_HOME
-    set -x RUSTUP_HOME "$HOME/.rustup"
+    set -x RUSTUP_HOME "$HOME"/.rustup
 end
 
 # cargo
 if test -z $CARGO_HOME
-    set -x CARGO_HOME "$HOME/.cargo"
+    set -x CARGO_HOME "$HOME"/.cargo
 end
 
 set -x PAGER less
 set -x LESS "--tilde -RFXS"
-set -x RIPGREP_CONFIG_PATH "$HOME/.config/ripgrep/config"
+set -x RIPGREP_CONFIG_PATH "$HOME"/.config/ripgrep/config
