e449552cde48755f71a1c65bdc162817951bca33
dangooddd <dangoodds@gmail.com>
2024-08-12 14:39:33 +0300

    chore: set font features and frondend for wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -9,5 +9,7 @@ require("appearance").merge(config)
 config.disable_default_key_bindings = true
 config.window_close_confirmation = "NeverPrompt"
 config.tab_bar_at_bottom = false
+config.front_end = "WebGpu"
+config.freetype_load_flags = "NO_HINTING"
 
 return config
