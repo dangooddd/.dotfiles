715066a0a166dc1b2323da16f16d8c6da0331040
dangooddd <dangoodds@gmail.com>
2024-08-20 02:54:57 +0300

    feat (shell): add auto_venv_toggle, refactor of auto_venv function

--- .config/fish/conf.d/prompt.fish
+++ .config/fish/conf.d/prompt.fish
@@ -15,17 +15,29 @@ end
 # ! only if venv name is ".venv" !
 function auto_venv --on-event fish_prompt
     set -l venv ./.venv/bin/activate.fish
-    if [ -z "$VIRTUAL_ENV" ]
-        if [ -e "$venv" ]
-            source $venv
-            set -g __auto_venv_path $PWD
-        end
-    else
-        if [ -n "$__auto_venv_path" ]
-            # check is $PWD contains $__auto_venv_path 
-            and not string match -e -q "$__auto_venv_path" "$PWD"
-            set -e __auto_venv_path
+
+    if [ -n "$VIRTUAL_ENV" ]
+        # check is $PWD contains (dirname "$VIRTUAL_ENV") (exit venv)
+        # or new venv is available (enter new venv)
+        if not string match -e -q (dirname "$VIRTUAL_ENV") "$PWD"
+            or begin [ -e "$venv" ]; and not [ (dirname "$VIRTUAL_ENV") -ef "$PWD" ]; end
             deactivate
         end
+    end
+
+    if [ -z "$VIRTUAL_ENV" ]
+        and [ -e "$venv" ]
+        and [ -z "$__auto_venv_stop" ]
+        source $venv
+    end
+end
+
+function auto_venv_toggle
+    if [ -z "$__auto_venv_stop" ]
+        set -g __auto_venv_stop true
+        echo "Stop auto_venv!"
+    else
+        set -e __auto_venv_stop
+        echo "Continue auto_venv!"
     end
 end
