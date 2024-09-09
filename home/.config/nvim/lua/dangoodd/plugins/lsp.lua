fb89a34dfa6ce0acea33b1c564a38b2c167ed7b6
dangooddd <dangoodds@gmail.com>
2024-09-09 09:45:00 +0300

    fix (nvim): increase limit of completion

--- home/.config/nvim/lua/dangoodd/plugins/lsp.lua
+++ home/.config/nvim/lua/dangoodd/plugins/lsp.lua
@@ -85,7 +85,7 @@ return {
                     -- truncate long lsp items
                     format = function(_, item)
                         local content = item.abbr
-                        local max_lenght = 25
+                        local max_lenght = 40
 
                         if #content > max_lenght then
                             item.abbr = vim.fn.strcharpart(content, 0, max_lenght) .. ".."
