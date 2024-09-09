3ecf8e9f9471a8fedcebea4df348b38db1ac8f86
dangooddd <dangoodds@gmail.com>
2024-09-09 14:57:52 +0300

    fix (nvim): fix git push keybind

--- home/.config/nvim/lua/dangoodd/plugins/git.lua
+++ home/.config/nvim/lua/dangoodd/plugins/git.lua
@@ -2,7 +2,7 @@ return {
     "tpope/vim-fugitive",
     config = function()
         vim.keymap.set("n", "<leader>ge", ":Git ")
-        vim.keymap.set("n", "<leader>gp", ":Gpush ")
+        vim.keymap.set("n", "<leader>gp", ":Git push ")
         vim.keymap.set("n", "<leader>ga", ":Git add ")
         vim.keymap.set("n", "<leader>gc", ":Git commit ")
         vim.keymap.set("n", "<leader>gl", ":vertical Git log ")
