9d5d9b57d2c1035c54995151b3cee89f0a196131
dangooddd <dangoodds@gmail.com>
2024-09-17 15:49:07 +0300

    feat (nvim): easy navigation in insert mode

--- home/.config/nvim/lua/dangoodd/plugins/pairs.lua
+++ home/.config/nvim/lua/dangoodd/plugins/pairs.lua
@@ -6,12 +6,21 @@ return {
     event = "VeryLazy",
     config = function()
         local pairs = require("nvim-autopairs")
+        local rule = require('nvim-autopairs.rule')
+        local cond = require('nvim-autopairs.conds')
         local cmp_pairs = require("nvim-autopairs.completion.cmp")
+
         pairs.setup()
         -- enable () after function complete
         require("cmp").event:on(
             "confirm_done",
             cmp_pairs.on_confirm_done()
         )
+
+        pairs.add_rules({
+            rule("$", "$",{ "tex", "latex" })
+                -- do not move right when repeat character
+                :with_move(cond.none())
+        })
     end,
 }
--- home/.config/nvim/lua/dangoodd/remap.lua
+++ home/.config/nvim/lua/dangoodd/remap.lua
@@ -1,8 +1,12 @@
 ---------------------------------------
 -- Keybinds
 ---------------------------------------
-vim.keymap.set("n", "<leader>bn", ":bnext<CR>", {})
-vim.keymap.set("n", "<leader>bp", ":bprev<CR>", {})
+vim.keymap.set("n", "<leader>bn", ":bnext<CR>")
+vim.keymap.set("n", "<leader>bp", ":bprev<CR>")
+vim.keymap.set("i", "<C-h>", "<Left>")
+vim.keymap.set("i", "<C-l>", "<Right>")
+vim.keymap.set("i", "<C-j>", "<Down>")
+vim.keymap.set("i", "<C-k>", "<Up>")
 
 local function map_snippet_jump(direction, keymap)
     local function snippet_jump()
