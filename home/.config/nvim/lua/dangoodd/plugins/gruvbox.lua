c932bcb6397796d2bc040041251f650da76cdeeb
dangooddd <dangoodds@gmail.com>
2024-10-13 21:08:28 +0300

    chore: add new wallpapers

---
+++ home/.config/nvim/lua/dangoodd/plugins/gruvbox.lua
@@ -0,0 +1,16 @@
+return {
+    "sainnhe/gruvbox-material", 
+    priority = 1000, 
+    config = function()
+        -- require("gruvbox").setup({
+        --     bold = false,
+        --     italic = {
+        --         strings = false,
+        --         emphasis = false,
+        --         comments = false,
+        --         operators = false,
+        --         folds = false,
+        --     },
+        -- })
+    end
+}
