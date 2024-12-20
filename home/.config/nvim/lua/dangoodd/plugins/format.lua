a83237f0aec680a6ba8b3ee70c35d5d3fa638fe0
dangooddd <dangoodds@gmail.com>
2024-12-21 01:56:36 +0300

    feat(nvim): add option to toggle autoformat

--- home/.config/nvim/lua/dangoodd/plugins/format.lua
+++ home/.config/nvim/lua/dangoodd/plugins/format.lua
@@ -3,10 +3,28 @@ return {
     event = "BufWritePre",  -- load before writing
     config = function()
         require("conform").setup({
-            format_on_save = {
-                timeout_ms = 500,
-                lsp_format = "fallback",
-            }
+            format_on_save = function(bufnr)
+                if vim.b[bufnr].disable_autoformat then
+                    return
+                end
+                return { 
+                    timeout_ms = 500, 
+                    lsp_format = "fallback" 
+                }
+            end,
         })
+
+        local function autoformat_toggle()
+            vim.b.disable_autoformat = not vim.b.disable_autoformat
+            local message = "autoformat: "
+            if vim.b.disable_autoformat then 
+                message = message.."disabled"
+            else
+                message = message.."enabled"
+            end
+            vim.notify(message, vim.log.levels.INFO)
+        end
+
+        vim.keymap.set("n", "<leader>at", autoformat_toggle)
     end,
 }
