69aa8056212b7d1cf84041411232f12281b1d20f
dangooddd <dangoodds@gmail.com>
2025-02-18 00:32:31 +0300

    fix: update lazy.nvim bootstrap script

--- home/.config/nvim/lua/dangoodd/lazy.lua
+++ home/.config/nvim/lua/dangoodd/lazy.lua
@@ -1,25 +1,21 @@
 ---------------------------------------
 -- Bootstrap lazy plugin manager
 ---------------------------------------
-local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazy_path) then
-    local out = vim.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazy_path,
-    }, { text = true }):wait()
-
-    if out.code ~= 0 then
-        vim.notify(
-            string.format("Failed to clone lazy.nvim:\n%s", out.stderr),
-            vim.log.levels.ERROR
-        )
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
     end
 end
-vim.opt.rtp:prepend(lazy_path)
+vim.opt.rtp:prepend(lazypath)
 
 -- lazy config
 require("lazy").setup({
