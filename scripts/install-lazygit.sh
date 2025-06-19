ad157b9c3a1795b69488ede0efc50254a6a09bfc
dangooddd <dangoodds@gmail.com>
2025-06-19 19:01:57 +0300

    feat: add lazygit installation script

---
+++ scripts/install-lazygit.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cd /tmp || exit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+install lazygit -D -t "$HOME"/.local/bin/
+rm lazygit lazygit.tar.gz
