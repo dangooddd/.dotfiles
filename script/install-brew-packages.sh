b54b26d42716edf4019e2d2713faaecb410b6f27
dangooddd <dangoodds@gmail.com>
2026-01-03 03:53:00 +0300

    Do not install devpod as dev package

--- script/install-brew-packages.sh
+++ script/install-brew-packages.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env sh
 
+if command -v /opt/homebrew/bin/brew &> /dev/null; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+if command -v /home/linuxbrew/.linuxbrew/bin/brew &> /dev/null; then
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
+
 xargs brew install < "$HOME"/.dotfiles/misc/packages/brew.txt
