6598b4b9b5db270ff1f5c9be82854c13c109cad1
dangooddd <dangoodds@gmail.com>
2026-01-26 23:11:49 +0300

    Add script to copy dotfiles dir to remote

---
+++ script/copy-dotfiles-remote.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+dotfiles="$(cd "../$(dirname "$0")" && pwd)"
+remote="$1"
+
+rsync -avz \
+  --exclude='*.jpg' \
+  --exclude='*.png' \
+  --exclude='.git/' \
+  "$dotfiles" "$remote"
+
