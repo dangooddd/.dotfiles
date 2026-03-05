55a60d5ded3859c522d584b7d987c23153e3ec0e
dangooddd <dangoodds@gmail.com>
2026-03-05 19:28:14 +0300

    Get rid of snippets

--- stow/nvim/.config/nvim/lua/dangoodd/plugins/completion.lua
+++ stow/nvim/.config/nvim/lua/dangoodd/plugins/completion.lua
@@ -1,24 +1,32 @@
 return {
-    -- snippets
-    {
-        "L3MON4D3/LuaSnip",
-        version = "*",
-        dependencies = { "rafamadriz/friendly-snippets" },
-        config = function()
-            require("luasnip.loaders.from_vscode").lazy_load()
-        end,
-    },
-
-    -- completion
-    {
-        "saghen/blink.cmp",
-        version = "*",
-        dependencies = {
-            "L3MON4D3/LuaSnip",
-            "folke/lazydev.nvim",
-        },
-        config = function()
-            require("blink.cmp").setup({
+    "saghen/blink.cmp",
+    version = "*",
+    dependencies = { "folke/lazydev.nvim" },
+    config = function()
+        require("blink.cmp").setup({
+            completion = {
+                list = {
+                    selection = {
+                        preselect = false,
+                        auto_insert = false,
+                    },
+                },
+                documentation = {
+                    auto_show = true,
+                    auto_show_delay_ms = 50,
+                },
+                menu = {
+                    draw = {
+                        columns = {
+                            { "label" },
+                            { "kind_icon", "kind", gap = 1 },
+                        },
+                    },
+                },
+            },
+            cmdline = {
+                enabled = true,
+                keymap = { preset = "inherit" },
                 completion = {
                     list = {
                         selection = {
@@ -26,60 +34,36 @@ return {
                             auto_insert = false,
                         },
                     },
-                    documentation = {
-                        auto_show = true,
-                        auto_show_delay_ms = 50,
-                    },
                     menu = {
-                        draw = {
-                            columns = {
-                                { "label" },
-                                { "kind_icon", "kind", gap = 1 },
-                            },
-                        },
+                        auto_show = true,
                     },
                 },
-                cmdline = {
-                    enabled = true,
-                    keymap = { preset = "inherit" },
-                    completion = {
-                        list = {
-                            selection = {
-                                preselect = false,
-                                auto_insert = false,
-                            },
-                        },
-                        menu = {
-                            auto_show = true,
-                        },
+            },
+            signature = {
+                enabled = true,
+            },
+            sources = {
+                default = { "lsp", "path", "snippets", "buffer", "lazydev" },
+                providers = {
+                    lazydev = {
+                        name = "LazyDev",
+                        module = "lazydev.integrations.blink",
+                        score_offset = 100,
                     },
                 },
-                signature = {
-                    enabled = true,
-                },
-                sources = {
-                    default = { "lsp", "path", "snippets", "buffer", "lazydev" },
-                    providers = {
-                        lazydev = {
-                            name = "LazyDev",
-                            module = "lazydev.integrations.blink",
-                            score_offset = 100,
-                        },
-                    },
-                },
-                snippets = { preset = "luasnip" },
-                keymap = {
-                    ["<C-y>"] = { "show", "show_documentation", "hide_documentation" },
-                    ["<C-g>"] = { "hide" },
-                    ["<C-p>"] = { "select_prev", "fallback" },
-                    ["<C-n>"] = { "select_next", "fallback" },
-                    ["<C-u>"] = { "scroll_documentation_up", "fallback" },
-                    ["<C-d>"] = { "scroll_documentation_down", "fallback" },
-                    ["<CR>"] = { "accept", "fallback" },
-                    ["<Tab>"] = { "snippet_forward", "fallback" },
-                    ["<S-Tab>"] = { "snippet_backward", "fallback" },
-                },
-            })
-        end,
-    },
+            },
+            snippets = { preset = "luasnip" },
+            keymap = {
+                ["<C-y>"] = { "show", "show_documentation", "hide_documentation" },
+                ["<C-g>"] = { "hide" },
+                ["<C-p>"] = { "select_prev", "fallback" },
+                ["<C-n>"] = { "select_next", "fallback" },
+                ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+                ["<C-d>"] = { "scroll_documentation_down", "fallback" },
+                ["<CR>"] = { "accept", "fallback" },
+                ["<Tab>"] = { "snippet_forward", "fallback" },
+                ["<S-Tab>"] = { "snippet_backward", "fallback" },
+            },
+        })
+    end,
 }
