a275183d9c9f1b8db272ea0f45e9f6210cca4e69
dangooddd <dangoodds@gmail.com>
2026-03-07 03:34:45 +0300

    Remove extra info from completion menu

--- stow/nvim/.config/nvim/lua/dangoodd/config.lua
+++ stow/nvim/.config/nvim/lua/dangoodd/config.lua
@@ -103,7 +103,16 @@ vim.api.nvim_create_autocmd("LspAttach", {
         local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 
         if client:supports_method("textDocument/completion") then
-            vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+            vim.lsp.completion.enable(true, client.id, args.buf, {
+                autotrigger = true,
+                convert = function(item)
+                    return {
+                        abbr = item.label,
+                        kind = item.kind and vim.lsp.protocol.CompletionItemKind[item.kind] or "",
+                        menu = "",
+                    }
+                end,
+            })
         end
     end,
 })
