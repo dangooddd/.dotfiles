cfc49876dc1fcdb80cae632ab9af385f63ebb866
dangooddd <dangoodds@gmail.com>
2026-03-28 21:54:16 +0300

    Fix bug in new link-dotfiles script

--- scripts/link-dotfiles.sh
+++ scripts/link-dotfiles.sh
@@ -3,7 +3,8 @@
 script_dir="$(dirname "$(realpath "$0")")"
 cd "$script_dir/../home"
 
-find "$PWD" \( -type f -o -type l \) -print0 | while IFS= read -r -d '' src; do
+find "$PWD" \( -type f -o -type l \) -print0 | while read -r -d '' src; do
     dst="${src/"$PWD"/"$HOME"}"
+    mkdir -p "$(dirname "$dst")"
     (set -x; ln -sfn "$src" "$dst")
 done
