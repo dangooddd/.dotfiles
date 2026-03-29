3f3a34bbc95629ec523b9745ac414e06e86f989f
dangooddd <dangoodds@gmail.com>
2026-03-29 13:03:28 +0300

    Use mktemp in yay installation script

--- scripts/install-yay.sh
+++ scripts/install-yay.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if ! command -v yay &>/dev/null; then
-    sudo pacman -S --noconfirm --needed git base-devel less
-    cd /tmp
-    git clone https://aur.archlinux.org/yay.git
-    cd yay
+    sudo pacman -S --noconfirm --needed git base-devel
+    yay="$(mktemp -d /tmp/yay-XXXXXXXX)"
+    git clone https://aur.archlinux.org/yay.git "$yay"
+    cd "$yay"
     makepkg -si
 fi
