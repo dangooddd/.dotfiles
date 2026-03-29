195cc5767c687fdf4ce4dbceaad1fa9ee5363cc6
dangooddd <dangoodds@gmail.com>
2026-03-29 12:55:53 +0300

    Use NONINTERACTIVE in brew installation script

--- scripts/install-brew.sh
+++ scripts/install-brew.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 if ! command -v brew &>/dev/null; then
-    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    NONINTERACTIVE=1 bash -c \
+        "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
